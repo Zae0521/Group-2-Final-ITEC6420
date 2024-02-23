6a9b5c59a9263aae5c7a76ddbbfee9c16be283cc
mehdymehdywhatwhatt <faik.mehdy@gmail.com>
2024-02-23 03:45:36 +0000

    refactored script.js and index.html to: expose all rest api endpoints iaw rest api itself; remove background; implement CREATE/UPDATE/DELETE for cars table in a way that is reproducible for branches table and inventory table.

--- frontend/script.js
+++ frontend/script.js
@@ -1,57 +1,99 @@
-var backendURL = "http://34.125.42.132:8080";
+// url for rest api.
+//
+const url_restapi = 'http://34.125.42.132:8080';
 
-window.onload = function(event)
-{
-	let command = {commandType:"GetCurrentDataset"};
-    PostToServer(command);
-};
+// api endpoints for `cars` table.
+// 
+const ep_car_create   = 'car/create';
+const ep_car_read_all = 'car/get';
+const ep_car_update   = 'car/update';
+const ep_car_id       = function(id) { return `car/${id}`; }
 
-function executeCMD(cmdType)
-{
-	var vehID = document.getElementById("vehID").value;
-	var vehYear = document.getElementById("vehYear").value;
-	var vehMake = document.getElementById("vehMake").value;
-	var vehType = document.getElementById("vehType").value;
-	var vehPrice = document.getElementById("vehPrice").value;
-	var braID = document.getElementById("braID").value;
-	var braName = document.getElementById("braName").value;
-	var braAddress = document.getElementById("braAddress").value;
-	var braCity = document.getElementById("braCity").value;
-	var braZip = document.getElementById("braZip").value;
-	var braPhone = document.getElementById("braPhone").value;
-	var braState = document.getElementById("braState").value;
-	var invID = document.getElementById("invID").value;
-	var invVeh  = document.getElementById("invVeh").value;
-	var invBra = document.getElementById("invBra").value;
-	var invQuantity  = document.getElementById("invQuantity").value;
-	
-	let command = {commandType:cmdType, vehID: vehID, vehYear: vehYear, vehMake: vehMake, vehType: vehType, vehPrice: vehPrice, braID: braID, braName: braName, braAddress: braAddress, braCity: braCity, braZip: braZip, braPhone: braPhone, braState: braState, invID: invID, invVeh: invVeh, invBra: invBra, invQuantity: invQuantity};
-    //alert(command.cmdType + ", " + command.vehID + ", " + command.vehYear + ", " + command.vehMake + ", " + command.vehType + ", " + command.vehPrice + ", " + command.braID + ", " + command.braName + ", " + command.braAddress + ", " + command.braCity + ", " + command.braZip + ", " + command.braPhone + ", " + command.braState + ", " + command.invID + ", " + command.invVeh + ", " + command.invBra + ", " + command.invQuantity);
-	
-	PostToServer(command);
+// api endpoints for `branches` table.
+//
+const ep_branch_create   = 'branch/create';
+const ep_branch_read_all = 'branch/get';
+const ep_branch_update   = 'branch/update';
+const ep_branch_id       = function(id) { return `branch/${id}`; }
+
+// api endpoints for `inventory` table.
+//
+const ep_inventory_create   = 'inventory/create';
+const ep_inventory_read_all = 'inventory/get';
+const ep_inventory_update   = 'inventory/update';
+const ep_inventory_id       = function(id) { return `inventory/${id}`; }
+
+// all element id's:
+// var vehID        = document.getElementById("vehID").value;
+// var vehYear      = document.getElementById("vehYear").value;
+// var vehMake      = document.getElementById("vehMake").value;
+// var vehType      = document.getElementById("vehType").value;
+// var vehPrice     = document.getElementById("vehPrice").value;
+//
+// var braID        = document.getElementById("braID").value;
+// var braName      = document.getElementById("braName").value;
+// var braAddress   = document.getElementById("braAddress").value;
+// var braCity      = document.getElementById("braCity").value;
+// var braZip       = document.getElementById("braZip").value;
+// var braPhone     = document.getElementById("braPhone").value;
+// var braState     = document.getElementById("braState").value;
+//
+// var invID        = document.getElementById("invID").value;
+// var invVeh       = document.getElementById("invVeh").value;
+// var invBra       = document.getElementById("invBra").value;
+// var invQuantity  = document.getElementById("invQuantity").value;
+// 
+
+function on_car(arg_http_verb) {
+
+  // setup.
+  var xhr = new XMLHttpRequest();
+  var http_body = null;
+
+  // state change.
+  xhr.onreadystatechange = function() { };
+
+  // build http body.
+  if (arg_http_verb === 'CREATE') {
+    xhr.open('POST', `${url_restapi}/${ep_car_create}`, true);
+    http_body = {
+      model : document.getElementById('vehMake').value,
+      type  : document.getElementById('vehType').value,
+      year  : document.getElementById('vehYear').value,
+      price : document.getElementById('vehPrice').value
+    };
+  }
+  else if (arg_http_verb === 'UPDATE') {
+    xhr.open('PUT', `${url_restapi}/${ep_car_update}`, true);
+    http_body = {
+      id    : document.getElementById('vehID').value,
+      model : document.getElementById('vehMake').value,
+      type  : document.getElementById('vehType').value,
+      year  : document.getElementById('vehYear').value,
+      price : document.getElementById('vehPrice').value,
+    };
+  }
+  else if (arg_http_verb === 'DELETE') {
+    const ep_delete_id = ep_car_id(document.getElementById('vehID').value);
+    xhr.open('DELETE', `${url_restapi}/${ep_delete_id}`, true);
+    http_body = { };
+  }
+  else {
+    console.log(`on_car: unsupported verb ${arg_http_verb}`);
+    return;
+  }
+
+  console.log(JSON.stringify(http_body));
+
+  // send.
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify(http_body));
 }
 
-function PostToServer(data)
-{
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function()
-	{
-		if (xhr.readyState == XMLHttpRequest.DONE)
-		{
-			if(data.commandType == "GetCurrentDataset")
-			{
-				jsonRecordsToTables(xhr.responseText);
-			}
-			else
-			{
-				//other response handlers, honestly though, refresh works as sa catch-all in this case so...
-				window.location.reload();
-			}
-		}
-	}
-	xhr.open("POST", backendURL, true);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify({ data }));
+function on_branch(arg_http_verb) {
+}
+
+function on_inventory(arg_http_verb) {
 }
 
 function jsonRecordsToTables(jsonRecords)
